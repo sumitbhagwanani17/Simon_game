85d859de4f9eb6c56b4339fea6f16f90bed07427
Sumit <sumit17b@gmail.com>
2020-04-27 19:43:36 -0400

    one round of the game
    can play one round of the game, till you get it wrong

---
+++ game.js
@@ -0,0 +1,97 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var game_pattern = [];
+var game_level = 0;
+var user_clicked_pattern = [];
+var user_entries = 0;
+var game_started = false;
+var game_over = false;
+
+// update the title
+function updateTitle(new_title) {
+    console.log("will update title to ", new_title);
+    $("#level-title").text(new_title);
+}
+
+// play the sound corresponding to the button that was clicked
+function playSound(pressed_button) {
+    var audio_for_button = new Audio("sounds/" + pressed_button + ".mp3");
+    audio_for_button.play();
+}
+
+// called when a new color is generated in the sequence, or the user clicks a button
+function buttonPressed(pressed_button) {
+    $("#" + pressed_button).addClass("pressed");
+    playSound(pressed_button);
+    setTimeout(function() {
+        $("#" + pressed_button).removeClass("pressed");
+    }, 150);
+}
+
+// generate the next color in the sequence
+function nextSequence() {
+    var randNum = Math.floor(Math.random() * 4);
+    var pressed_button = buttonColors[randNum];
+    game_pattern.push(pressed_button);
+    console.log(game_pattern);
+    buttonPressed(pressed_button);
+    game_level++;
+    updateTitle("Level " + (game_level));
+}
+
+// return page to normal background, once user gets the sequence wrong
+function resetPage(params) {
+    console.log("reseting background");
+    $("body").removeClass("game-over");
+    game_over = true;
+}
+
+// once the user gets the sequence wrong
+function gameOver() {
+    $("body").addClass("game-over");
+    updateTitle("Game Over, Press Any Key to Restart");
+    setTimeout(function() {
+        resetPage();
+    }, 200);
+}
+
+// check answer once the user has clicked the color buttons
+function checkAnswer(pressed_button) {
+    if (pressed_button == game_pattern[user_entries]) {
+        //console.log("done with ", user_entries);
+    } else {
+        //console.log("failed on ", user_entries);
+        gameOver();
+    }
+    user_entries++;
+    if (user_entries == game_level) {
+        user_entries = 0;
+        setTimeout(function() {
+            nextSequence();
+        }, 1000);
+    }
+}
+
+// user clicking one of the color buttons
+$(".btn").click(function(e) {
+    var pressed_button = $(this).attr("id");
+    buttonPressed(pressed_button);
+    checkAnswer(pressed_button);
+});
+
+// start new game
+function startGame() {
+    console.log("key press detected");
+    if (game_over) {
+        location.reload();
+    } else {
+        nextSequence();
+    }
+}
+
+// user clicking any key on the keyboard
+$(document).keydown(function(e) {
+    if (!game_started) {
+        game_started = true;
+        startGame();
+    }
+});
